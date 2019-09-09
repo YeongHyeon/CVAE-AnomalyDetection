3753b5c162ffdbd69254e92ea5d4d964fd8c3ec6
박영현(Yeonghyeon Park)/IoT센서솔루션사업팀/SKP <young200405@gmail.com>
2019-09-09 13:49:57 +0900

    20190909

--- source/tf_process.py
+++ source/tf_process.py
@@ -73,6 +73,22 @@ def latent_plot(latent, y, n, savename=""):
     plt.savefig(savename)
     plt.close()
 
+def boxplot(contents, savename=""):
+
+    data, label = [], []
+    for cidx, content in enumerate(contents):
+        data.append(content)
+        label.append("class-%d" %(cidx))
+
+    plt.clf()
+    fig, ax1 = plt.subplots()
+    bp = ax1.boxplot(data, showfliers=True)
+    ax1.set_xticklabels(label, rotation=45)
+
+    plt.tight_layout()
+    plt.savefig(savename)
+    plt.close()
+
 def training(sess, saver, neuralnet, dataset, epochs, batch_size, normalize=True):
 
     print("\nTraining to %d epochs (%d of minibatch size)" %(epochs, batch_size))
@@ -122,7 +138,7 @@ def training(sess, saver, neuralnet, dataset, epochs, batch_size, normalize=True
             _, summaries = sess.run([neuralnet.optimizer, neuralnet.summaries], \
                 feed_dict={neuralnet.x:x_tr, neuralnet.batch_size:x_tr.shape[0]}, \
                 options=run_options, run_metadata=run_metadata)
-            restore, kld, loss = sess.run([neuralnet.mean_restore, neuralnet.mean_kld, neuralnet.loss], \
+            restore_loss, kld, loss = sess.run([neuralnet.mean_restore, neuralnet.mean_kld, neuralnet.loss], \
                 feed_dict={neuralnet.x:x_tr, neuralnet.batch_size:x_tr.shape[0]})
             summary_writer.add_summary(summaries, iteration)
 
@@ -130,7 +146,7 @@ def training(sess, saver, neuralnet, dataset, epochs, batch_size, normalize=True
             if(terminator): break
 
         print("Epoch [%d / %d] (%d iteration)  Restore:%.3f, KLD:%.3f, Total:%.3f" \
-            %(epoch, epochs, iteration, restore, kld, loss))
+            %(epoch, epochs, iteration, restore_loss, kld, loss))
         saver.save(sess, PACK_PATH+"/Checkpoint/model_checker")
         summary_writer.add_run_metadata(run_metadata, 'epoch-%d' % epoch)
 
@@ -148,16 +164,17 @@ def test(sess, saver, neuralnet, dataset, batch_size):
 
     loss_list = []
     while(True):
-        x_te, y_te, terminator = dataset.next_test(batch_size) # y_te does not used in this prj.
-        loss = sess.run(neuralnet.loss, \
+        x_te, y_te, terminator = dataset.next_test(1) # y_te does not used in this prj.
+        restore_loss = sess.run(neuralnet.mean_restore, \
             feed_dict={neuralnet.x:x_te, neuralnet.batch_size:x_te.shape[0]})
-        loss_list.append(loss)
+        if(y_te[0] == 1):
+            loss_list.append(restore_loss)
 
         if(terminator): break
 
     loss_list = np.asarray(loss_list)
     loss_avg, loss_std = np.average(loss_list), np.std(loss_list)
-    outbound = loss_avg + (loss_std * 1.5)
+    outbound = loss_avg + (loss_std * 3)
     print("Loss  avg: %.3f, std: %.3f" %(loss_avg, loss_std))
     print("Outlier boundary: %.3f" %(outbound))
 
@@ -165,11 +182,14 @@ def test(sess, saver, neuralnet, dataset, batch_size):
     fcsv.write("class, loss, outlier\n")
     testnum = 0
     z_enc_tot, y_te_tot = None, None
+    loss4box = [[], [], [], [], [], [], [], [], [], []]
     while(True):
         x_te, y_te, terminator = dataset.next_test(1) # y_te does not used in this prj.
 
-        x_restore, z_enc, loss = sess.run([neuralnet.x_hat, neuralnet.z_enc, neuralnet.loss], \
+        x_restore, z_enc, restore_loss = sess.run([neuralnet.x_hat, neuralnet.z_enc, neuralnet.mean_restore], \
             feed_dict={neuralnet.x:x_te, neuralnet.batch_size:x_te.shape[0]})
+
+        loss4box[y_te[0]].append(restore_loss)
 
         if(z_enc_tot is None):
             z_enc_tot = z_enc
@@ -178,21 +198,25 @@ def test(sess, saver, neuralnet, dataset, batch_size):
             z_enc_tot = np.append(z_enc_tot, z_enc, axis=0)
             y_te_tot = np.append(y_te_tot, y_te, axis=0)
 
-        outcheck = loss > outbound
-        fcsv.write("%d, %.3f, %r\n" %(y_te, loss, outcheck))
+        outcheck = restore_loss > outbound
+        fcsv.write("%d, %.3f, %r\n" %(y_te, restore_loss, outcheck))
 
-        canvas = np.ones((x_te[0].shape[0], x_te[0].shape[1]*2, x_te[0].shape[2]), np.float32)
-        canvas[:, :x_te[0].shape[1], :] = x_te[0]
-        canvas[:, x_te[0].shape[1]:, :] = x_restore[0]
+        [h, w, c] = x_te[0].shape
+        canvas = np.ones((h, w*3, c), np.float32)
+        canvas[:, :w, :] = x_te[0]
+        canvas[:, w:w*2, :] = x_restore[0]
+        canvas[:, w*2:, :] = (x_te[0]-x_restore[0])**2
         if(outcheck):
-            plt.imsave(os.path.join("test", "outbound", "%08d.png" %(testnum)), gray2rgb(gray=canvas))
+            plt.imsave(os.path.join("test", "outbound", "%08d-%08d.png" %(testnum, int(restore_loss))), gray2rgb(gray=canvas))
         else:
-            plt.imsave(os.path.join("test", "inbound", "%08d.png" %(testnum)), gray2rgb(gray=canvas))
+            plt.imsave(os.path.join("test", "inbound", "%08d-%08d.png" %(testnum, int(restore_loss))), gray2rgb(gray=canvas))
 
         testnum += 1
 
         if(terminator): break
 
+    boxplot(contents=loss4box, savename="test-box.png")
+
     if(neuralnet.z_dim == 2):
         latent_plot(latent=z_enc_tot, y=y_te_tot, n=dataset.num_class, \
-            savename=os.path.join("test.png"))
+            savename=os.path.join("test-latent.png"))
